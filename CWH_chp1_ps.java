bd268d3ee66a32a2de90f2b582c75a9716d360d2
Anshul Darediya <97527781+AnshulDarediya@users.noreply.github.com>
2024-01-15 23:00:07 +0530

    Add files via upload

---
+++ CWH_chp1_ps.java
@@ -0,0 +1,38 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class CWH_chp1_ps {
+    public static void main(String[] args) {
+
+//        Question 1
+//        int num1=20;
+//        int num2=30;
+//        int num3=19;
+//        System.out.println(num1+num2+num3);
+
+//        Question 2
+//        int mark1=92;
+//        int mark2=98;
+//        int mark3=55;
+//        float cgpa=(mark1+mark2+mark3)/30.0f;
+//        System.out.println(cgpa);
+
+//        Question 3
+//        System.out.println("What is Your Name ?");
+//        Scanner sc = new Scanner(System.in);
+//        String name=sc.next();
+//        System.out.println("Hello "+ name +" Have a Good Day");
+
+
+//        Question 4
+//        int km=100;
+//        float mul_Fact=0.621371f;
+//        System.out.println(km*mul_Fact);
+
+//        Question 5
+//        System.out.println("Enter a Interger");
+//        Scanner sc=new Scanner(System.in);
+//        Boolean flag=sc.hasNextInt();
+//        System.out.println(flag);
+    }
+}
