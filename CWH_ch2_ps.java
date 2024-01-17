31ddc046b271394bc025f32587ddf4ef3ff68a2d
Anshul Darediya <97527781+AnshulDarediya@users.noreply.github.com>
2024-01-17 13:44:47 +0530

    Add files via upload

---
+++ CWH_ch2_ps.java
@@ -0,0 +1,22 @@
+public class CWH_ch2_ps {
+
+    public static void main(String[] args) {
+        //Question 1
+//        float a = 7/4.0f * 9/2.0f;
+//        System.out.println(a);
+
+        //Question 2
+        char grade = 'B';
+//        grade = (char)(grade + 8);
+//        System.out.println(grade);
+//        // Decrypting the grade
+//        grade = (char)(grade - 8);
+//        System.out.println(grade);
+
+        //Question 3
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        System.out.println(a>8);
+//        System.out.println(7*49/7+35/7);
+    }
+}
