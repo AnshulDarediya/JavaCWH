4f237365ab779d85ceec7826d396d42c698a399d
Anshul Darediya <97527781+AnshulDarediya@users.noreply.github.com>
2024-01-17 13:33:52 +0530

    Add files via upload

---
+++ CWH_ch3_ps.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class CWH_ch3_ps {
+    public static void main(String[] args) {
+        // Problem 1
+//        String name = "Anshul Darediya";
+//        name = name.toLowerCase();
+//        System.out.println(name);
+
+        // Problem 2
+//        String text = "To My     Friend";
+//        text = text.replace(" ", "_");
+//        System.out.println(text);
+
+        // Problem 3
+//        String letter = "Dear <|name|>, Thanks a lot!";
+//        Scanner sc= new Scanner(System.in);
+//        String str=sc.next();
+//        letter = letter.replace("<|name|>", str);
+//        System.out.println(letter);
+
+        // Problem 4
+//        String myString = "This string contains double and  triple spaces";
+//        System.out.println(myString.indexOf("  "));
+//        System.out.println(myString.indexOf("   "));
+
+        // Problem 5
+//        String myLetter = "Dear Anshul,\n\tThis Java Course is Nice.\nThanks!";
+//        System.out.println(myLetter);
+
+    }
+}
